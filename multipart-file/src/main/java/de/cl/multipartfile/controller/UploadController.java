580bc8e3881da29f2171d754779f42c049bdd6a1
Syoxz <marcel240@hotmail.com>
2022-05-04 15:25:40 +0200

    Upload Button gibt Hello World Datei aus

--- multipart-file/src/main/java/de/cl/multipartfile/controller/UploadController.java
+++ multipart-file/src/main/java/de/cl/multipartfile/controller/UploadController.java
@@ -1,14 +1,29 @@
 package de.cl.multipartfile.controller;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class UploadController {
 
-	@GetMapping("")
-    public String showUebungenList1(Model model) {
+@GetMapping("")
+    public String showFileUploader() {
         return "index";
 }
-}
+@PostMapping ("/save")
+	public String fileUpload () throws IllegalStateException, IOException {
+	
+		MultipartFile multipartFile = new MockMultipartFile("sourceFile.tmp", "Hello World".getBytes());
+		File file = new File("src/main/resources/targetFile.tmp");
+		multipartFile.transferTo(file);
+
+		return "index";
+		}
+	}
+
