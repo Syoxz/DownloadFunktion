3c832db44322860ac27dc83befa9730f9c100728
Syoxz <marcel240@hotmail.com>
2022-05-05 12:11:18 +0200

    tomcat integration

---
+++ multipart-file/src/test/java/de/cl/multipartfile/MultipartFileApplicationTests.java
@@ -0,0 +1,13 @@
+package de.cl.multipartfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipartFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
